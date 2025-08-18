4d6c70cee8c7758827d67e67cc5fe97124a8d616
bagulvijay <vp927630@gmail.com>
2025-08-18 15:06:09 +0530

    Added gameActive flag to disable clicks during 2s final win delay

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let userCount=0;
 let compCount=0;
+let gameActive = true;
 const choice=document.querySelectorAll(".choice");
 const msg=document.querySelector(".msg");
 const userNo=document.querySelector(".userNo");
@@ -20,13 +21,14 @@ const reset=()=>{
     userNo.innerText=userCount;
     compCount=0;
     compNo.innerText=compCount;
-    
+    msg.innerText="Play Your Move";
+    removeBgColour();
     
 }
 resetBtn.addEventListener("click",()=>{
     reset();
-    msg.innerText="Play Your Move";
-    removeBgColour();
+    
+    
     msg.classList.add("msg");
 })
 const bgGreen=()=>{
@@ -40,14 +42,16 @@ const bgRed=()=>{
 const checkFinalWin=(userCount,compCount)=>{
     if(userCount==5){
         bgGreen();
-        msg.innerText="YOU WIN"
-        setTimeout(()=> reset(), 1000);
+        msg.innerText="YOU WIN";
+        gameActive=false;
+        setTimeout(()=>{reset();gameActive=true;} , 2000);
         
     }
     else if(compCount==5){
         bgRed();
         msg.innerText="YOU LOST, Computer Win";
-        setTimeout(()=> reset(), 1000);
+        gameActive=false;
+        setTimeout(()=>{reset();gameActive=true;} , 2000);
         
     }
 }
@@ -76,6 +80,7 @@ const removeScore=()=>{
 }
 choice.forEach((choice)=>{
     choice.addEventListener("click",()=>{
+        if(!gameActive)return;
         removeScore();
         const userChoice=choice.getAttribute("id");
         const compChoice=getCompChoice();
